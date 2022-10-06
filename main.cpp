c511a05d08f3652d133464d4422ff90aa9c7e72e
Declan <dkutscher10@gmail.com>
2022-10-05 20:53:26 -0400

    load csvc, print matrix use fstream, iostream

--- main.cpp
+++ main.cpp
@@ -2,18 +2,12 @@
 #include <string>
 #include <fstream>
 #include <vector>
-
 using namespace std;
 
-std::vector<std::vector<std::string> > matrix;
+std::vector<std::vector<string>> matrix(1001, std::vector<string>(4));
 
 int main(int argc, char const *argv[])
 {
-    string file_name;
-
-    cout << "Please enter the filename with extension. ( ie: \"dataset.csv\" ) ";
-    cin >> file_name;
-
     std::string line = "";
     std::string temp = "";
 
@@ -21,27 +15,36 @@ int main(int argc, char const *argv[])
     int col = 0;
 
     ifstream in_file;
-    in_file.open(file_name);
+
+    in_file.open("dataset.csv");
 
     if (in_file.is_open())
     {
+        // cout << "file is open\n";
 
         int k = 0;
         int j = 0;
         while (getline(in_file, line))
         {
+            // cout << "Getting line...\n";
+
             if (line != "")
             {
+                // cout << "line is not null \n";
                 j = 0;
                 line = line + ",";
+                // cout << "top of for loop\n";
                 for (int i = 0; i < line.length(); i++)
                 {
+                    // cout << "inside for loop   "<< "i :" << i << " row: " << k << " col: " << j << "\n";
                     if (line[i] != ',')
                     {
+                        // cout << "appending temp: " << temp << "\n";
                         temp += line[i];
                     }
                     else
                     {
+                        // cout << "reached , seperator \n";
                         matrix[k][j] = temp;
                         temp = "";
                         j++;
@@ -55,15 +58,19 @@ int main(int argc, char const *argv[])
     {
         cout << "failed to open file.";
     }
+    cout << "Matrix loaded, printing... \n";
+    cout << "matrix rows: " << matrix.size() << std::endl;
+    cout << "matrix columns: " << matrix[0].size() << std::endl;
 
     for (unsigned i = 0; i < matrix.size(); i++)
     {
         for (unsigned j = 0; j < matrix[0].size(); j++)
         {
-            string output = matrix[i][j];
+            string output = matrix[i][j] + " ";
             cout << output;
         }
+        cout << std::endl;
     }
-
+    cout << "Done, exiting \n";
     return 0;
 }
